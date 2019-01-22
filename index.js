7c51eef156447987c66bb1afafff1a00441725ee
JuiceboxJones <jderhalli@gmail.com>
2019-01-22 08:44:29 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+'use strict';
+
+/*
+questions outline
+
+Which of these songs was a hit by Whitney Houston in 1990?
+--I Will Always Love You
+-exhale
+-Im every woman
+-the greatest love of all
+
+Which r&b male/group is known to beg in his/their music:
+--Keith Sweat
+-R. Kelly
+-Tony Toni Tony
+- Jodeci
+
+Which 90s R&B song was considered the first mashup to be on the billboards top 100.
+-- "Right here/Human nature remix" by SWV
+- 'Scream' by Michael Jackson/Janet Jackson
+- 'I'll be missing you' Puff Daddy and the family
+- 'Youre all I need' by Mary J Blige feat Method Man
+
+Mariah Carey had a hit song in 1995 with what singer/group?
+-- Boyz II Men
+-Babyface
+-Whitney Houston
+-Luther Vandross
+
+Escapade, Come Back to Me and Black Cats are songs from what Janet Jackson album?
+-- Rhythym Nation 1814
+- Janet
+- Control
+- All for you
+
+*/
